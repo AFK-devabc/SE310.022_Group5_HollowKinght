ad8377f0115dbf59835e30e252477a5175b921ec
TendouHakai <thanhconghoahai@gmail.com>
2023-11-27 08:56:44 +0700

    create BaseObject and PlayObject in Script

---
+++ Assets/Script/BaseObject.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseObject : MonoBehaviour, Subcriber
+{
+    public float Damage;
+    public float Speed;
+    [SerializeField] protected Vector3 velocity;
+    [SerializeField] public Transform Target;
+    [SerializeField] protected int State;
+    public bool isMove;
+    public bool isDead;
+    public bool isRight;
+    public bool isStun;
+
+    [Header("----------Component----------")]
+    [SerializeField] public Rigidbody2D rb;
+    [SerializeField] public SpriteRenderer render;
+    [SerializeField] public Animator ani;
+    public bool isGrounded;
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        GameStateManager.getInstance().publisherGameState.subcribe(this);
+        isMove = true;
+        isDead = false;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (isMove)
+            Move();
+    }
+
+    public virtual void Move()
+    {
+        transform.position += Speed * velocity * Time.deltaTime;
+    }
+
+    public virtual void setTarget(Transform target)
+    {
+        this.Target = target;
+    }
+
+    public virtual void setState(int state)
+    {
+        this.State = state;
+    }
+
+    public virtual void takeDamage(float damage)
+    {
+
+    }
+
+    public virtual void Dead()
+    {
+        GameStateManager.getInstance().publisherGameState.unsubcribe(this);
+    }
+
+    public virtual void flip()
+    {
+        if (isRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    // subcribe
+
+    public void OnDestroy()
+    {
+        if (GameStateManager.getInstance() != null)
+        {
+            GameStateManager.getInstance().publisherGameState.unsubcribe(this);
+        }
+    }
+
+    public virtual void update(int state)
+    {
+        if (state == (int)Game_State.Pause)
+        {
+            if (rb != null)
+            {
+                rb.velocity = Vector2.zero;
+                rb.isKinematic = true;
+            }
+
+            if (ani != null)
+            {
+                ani.enabled = false;
+            }
+
+            enabled = false;
+        }
+        else
+        {
+            if (rb != null)
+                rb.isKinematic = false;
+
+            if (ani != null)
+            {
+                ani.enabled = true;
+            }
+
+            enabled = true;
+        }
+    }
+}
---
+++ Assets/Script/PlayObject.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayObject : BaseObject
+{
+    [SerializeField] public float currentHP;
+    [SerializeField] public float MaxHP;
+    [SerializeField] public bool isAttack;
+
+    protected override void Start()
+    {
+        base.Start();
+        currentHP = MaxHP;
+        isAttack = false;
+        isRight = true;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public virtual void Attack()
+    {
+
+    }
+
+    public override void takeDamage(float damage)
+    {
+        currentHP -= damage;
+
+        if (currentHP <= 0)
+        {
+            Dead();
+        }
+    }
+
+    public override void Dead()
+    {
+        base.Dead();
+        this.isDead = true;
+    }
+}
