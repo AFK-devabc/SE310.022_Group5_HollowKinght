3575f1239d818d6bb04a81a9e863cbc7b3c77d83
Hoàng Mai <hgmaidg@gmail.com>
2023-12-26 15:23:14 +0700

    MenuStar + pauseGame + pauseStart
    Complete coding menu start game and add music start/ music combat + Complete code pause game +Complete coding pauseMenu in gamePlay

--- Assets/Script/Camera/CameraFollow.cs
+++ Assets/Script/Camera/CameraFollow.cs
@@ -7,10 +7,45 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Transform target;
     [SerializeField] float speed;
     [SerializeField] Vector3 offset;
-    // Start is called before the first frame update
+
+    // start Scene
+    [Header("----------Start scene----------")]
+    [SerializeField] Player playerFrefabs;
+    [SerializeField] PlayerControl playerControlFrefabs;
+    [SerializeField] UIManager uIManagerFrefabs;
+    [SerializeField] SceneLoader sceneLoaderFrefabs;
+
+    [Header("----------Viewport Size----------")]
+    [SerializeField] Vector2 MaxPosition;
+    [SerializeField] Vector2 MinPosition;
+
+    private void Awake()
+    {
+
+    }
     void Start()
     {
-        
+        if (target != null) return;
+        Player player = GameObject.FindObjectOfType<Player>();
+        if (player == null)
+        {
+            Vector3 pos = transform.position;
+            pos.z = -0.01f;
+            player = Instantiate(playerFrefabs, pos, Quaternion.identity);
+            target = player.transform;
+
+            Instantiate(playerControlFrefabs, Vector3.zero, Quaternion.identity);
+            Instantiate(uIManagerFrefabs, Vector3.zero, Quaternion.identity);
+            Instantiate(sceneLoaderFrefabs, Vector3.zero, Quaternion.identity);
+
+            GameStateManager.getInstance().addSubcriberDontDestroy();
+        }
+        else
+        {
+            target = player.transform;
+        }
+        SoundManager.getInstance().PlayMusic("AbyssMusic01");
+        MinimapManager.getInstance().setPlayer(player);
     }
 
     // Update is called once per frame
@@ -21,14 +56,10 @@ public class CameraFollow : MonoBehaviour
         Vector3 smoothPosition = Vector3.Slerp(transform.position, desiredPosition, speed * Time.deltaTime);
 
         smoothPosition.x = desiredPosition.x;
+
+        desiredPosition.x = Mathf.Clamp(desiredPosition.x, MinPosition.x, MaxPosition.x);
+        desiredPosition.y = Mathf.Clamp(desiredPosition.y, MinPosition.y, MaxPosition.y);
+
         transform.position = desiredPosition;
     }
-
-    //private void FixedUpdate()
-    //{
-    //    Vector3 desiredPosition = target.position + offset;
-    //    Vector3 smoothPosition = Vector3.Slerp(transform.position, desiredPosition, speed * Time.deltaTime);
-
-    //    transform.position = smoothPosition;
-    //}
 }
--- Assets/Script/Manager/UIManager.cs
+++ Assets/Script/Manager/UIManager.cs
@@ -1,8 +1,10 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class UIManager : MonoBehaviour
+public class UIManager : MonoBehaviour, Subcriber
 {
     private static UIManager instance;
 
@@ -18,23 +20,90 @@ public class UIManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+
+        DontDestroyOnLoad(gameObject);
     }
 
     [Header("----------MENU----------")]
-    public GameObject GuideMenu;
+    public GameObject HUD;
+    public GameObject Inventory;
+    public GameObject Map;
+    public GameObject PauseMenu;
 
     void Start()
     {
-        GuideMenu.SetActive(false);
+        HUD.SetActive(true);
+        Inventory.SetActive(false);
+        PauseMenu.SetActive(false);
+
+        updateCurrentMap();
     }
 
-    public void OpenGuideMenu()
+    private void Update()
     {
-        GuideMenu.SetActive(true);
+        if(Input.GetKeyDown(KeyCode.I) && !Inventory.activeInHierarchy)
+        {
+            OpenIMenu();
+        }
+
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (Inventory.activeInHierarchy)
+            {
+                CloseIMenu();
+            }
+            else
+            {
+                OpenPauseMenu();
+            }
+        }
     }
 
-    public void CloseGuideMenu()
+    public void closeAllMenu()
     {
-        GuideMenu.SetActive(false);
+        Inventory.SetActive(false );
+    }
+
+    public void OpenIMenu()
+    {
+        Inventory.SetActive(true);
+        PlayerControl.getInstance().isInteract = true;
+    }
+
+    public void CloseIMenu()
+    {
+        Inventory.SetActive(false);
+        PlayerControl.getInstance().isInteract = false;
+    }
+
+    public void OpenPauseMenu()
+    {
+        PauseMenu.SetActive(true);
+        GameStateManager.getInstance().setState(Game_State.Pause);
+    }
+
+    public void ClosePauseMenu()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    // Map
+    public void addMarker(int id, Vector3 pos)
+    {
+        Map.GetComponent<MapMenuControler>().addMarker(id, pos);
+    }
+
+    public void updateCurrentMap()
+    {
+        Map.GetComponent<MapMenuControler>().updateCurrentMap(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    //subcribe
+    public void update(int state)
+    {
+        if (state == (int)Game_State.BacktoMenu)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
